e5d71b6c3f5ed7646394c6d613ef7d2f02fc7b83
propscodes-source <propscodes@gmail.com>
2026-03-09 13:31:06 +0700

    feat: Implement AdminDashboard component for comprehensive content management of activities, hero images, projects, and videos.

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -21,6 +21,7 @@ import {
   Film,
   CheckCircle,
 } from "lucide-react";
+
 import { useNavigate } from "react-router-dom";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
