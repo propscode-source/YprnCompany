960780d85272ecb5455cc7c58b9f52e83e565787
propscodes-source <propscodes@gmail.com>
2026-03-11 21:49:46 +0700

    feat: Set up the main React application entry point with routing and global context providers.

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import { LanguageProvider } from "./context/LanguageContext";
 import "./index.css";
 import App from "./App.jsx";
 
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <LanguageProvider>
         <AuthProvider>
@@ -15,5 +16,5 @@ createRoot(document.getElementById("root")).render(
         </AuthProvider>
       </LanguageProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 );
